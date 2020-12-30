ff199ae912c4419143325705f5732b02df9e5fd2
zhangminghui6106 <zhangminghui6106@navinfo.com>
2020-12-30 11:02:05 +0800

    修改api_gateway

--- svc/shared/docker/python-flask-grpc/build.sh
+++ svc/shared/docker/python-flask-grpc/build.sh
@@ -1,3 +1,3 @@
-IMAGE=itinfomation/delinkcious-python-flask-grpc:0.1
-docker build . -t $IMAGE
-docker push $IMAGE
+#IMAGE=itinfomation/delinkcious-python-flask-grpc:0.1
+#docker build . -t $IMAGE
+#docker push $IMAGE
